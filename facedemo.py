a212c7e96ab70a27852af90a2f79e0819b2d736c
Tim Sherratt <tim@discontents.com.au>
2016-11-04 23:45:11 +1100

    Add wsgi

--- facedemo.py
+++ facedemo.py
@@ -67,4 +67,4 @@ def check_face_job(job_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from facedemo import app as application
+
+if __name__ == "__main__":
+    application.run()
